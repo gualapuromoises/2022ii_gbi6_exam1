bd3f0330caf6c31e239d5a36a215c005648b7ebf
Jeanpiee.cerda <jeanpetrickc@gmail.com>
2022-11-29 22:23:55 -0500

    cambios nuevo
    JeanPierre hizo cambios

--- code/role.sh
+++ code/role.sh
@@ -2,12 +2,14 @@ echo " Pregunta 1"
 echo " "
 #Número de registros de cada rol del gen
 echo "Núm. de registros de cada rol del gen:"
-cat ../data/cancermine.csv | cut -d "," -f 1 | tail -n +2 | wc -l
+echo ../data/cancermine.csv | cut -d "," -f 1 | tail -n +2 | wc -l >> ../data/roles.txt
 
 #Especie y conteo más alto
-echo "Especie y conteo más alto:"
-cat ../data/cancermine.csv | cut -d "," -f 1 | tail -n +2 | sort -n | uniq -c | sort -r | head -n 1
+echo "Especie y conteo más alto:"  >> ../data/roles.txt
 
-#Especie y conteo más bajo
-echo "Especie y conteo más bajo:"
-cat ../data/cancermine.csv | cut -d "," -f 1 | tail -n +2 | sort -n | uniq -c | sort -r | tail -n
+echo ../data/cancermine.csv | cut -d "," -f 1 | tail -n +2 | grep "Oncogene" | uniq -c | sort >> ../data/roles.txt
+
+#Especie y conteo más bajo 
+echo "Especie y conteo más bajo:"  >> ../data/roles.txt
+
+echo ../data/cancermine.csv | cut -d "," -f 1 | tail -n +2 | grep "Driver" | uniq -c | sort >> ../data/roles.txt
