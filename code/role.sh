61296220f3ce2f252ae45e6bc863b3bd44c11db8
Jeanpiee.cerda <jeanpetrickc@gmail.com>
2022-11-29 23:16:20 -0500

    Cerda Jean-Pierre ha contado los roles de los genes

--- code/role.sh
+++ code/role.sh
@@ -1,1 +1,16 @@
+#Número de registros de cada rol del gen
+
+echo "Número de registros de cada rol del gen:" >>../data/roles.txt
+
+tail -n +2 ../data/cancermine.csv | cut -d "," -f 1| sort |uniq -c >> ../data/roles.txt
+
+
+#Especie y conteo más alto
+echo "Especie y conteo más alto:" >>../data/roles.txt
+cat ../data/cancermine.csv | cut -d "," -f 1 | tail -n +2 | sort -n | uniq -c | sort -r | head -n 1 >>../data/roles.txt
+
+#Especie y conteo más bajo
+echo "Especie y conteo más bajo:" >>../data/roles.txt
+cat ../data/cancermine.csv | cut -d "," -f 1 | tail -n +2 | sort -n | uniq -c | sort -r | tail -n 1 >>../data/roles.txt
+
 
